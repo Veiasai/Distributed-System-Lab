23c9d0b6dbdca027b8cfe0270243065bcb5d6dfd
veia-mac <917971502@qq.com>
2019-03-06 11:45:39 +0800

    lab 1

---
+++ rdt/rdt_receiver.cc
@@ -0,0 +1,99 @@
+/*
+ * FILE: rdt_receiver.cc
+ * DESCRIPTION: Reliable data transfer receiver.
+ * NOTE: This implementation assumes there is no packet loss, corruption, or 
+ *       reordering.  You will need to enhance it to deal with all these 
+ *       situations.  In this implementation, the packet format is laid out as 
+ *       the following:
+ *       
+ *       |<-  1 byte  ->|<-             the rest            ->|
+ *       | payload size |<-             payload             ->|
+ *
+ *       The first byte of each packet indicates the size of the payload
+ *       (excluding this single-byte header)
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rdt_struct.h"
+#include "rdt_receiver.h"
+
+#include <algorithm>
+
+static const int window_size = 10;
+static struct packet * window[window_size];
+static int cur;
+extern volatile int send_cur;
+
+
+/* receiver initialization, called once at the very beginning */
+void Receiver_Init()
+{
+    fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
+    memcpy(window, 0, sizeof(window));
+    cur = 0;
+}
+
+/* receiver finalization, called once at the very end.
+   you may find that you don't need it, in which case you can leave it blank.
+   in certain cases, you might want to use this opportunity to release some 
+   memory you allocated in Receiver_init(). */
+void Receiver_Final()
+{
+    fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
+}
+
+/* event handler, called when a packet is passed from the lower layer at the 
+   receiver */
+void Receiver_FromLowerLayer(struct packet *pkt)
+{
+    /* 1-byte header indicating the size of the payload */
+    int header_size = 8;
+    // check id
+    int re_id = *(int*)(pkt->data+4);
+
+    // check ack
+    if (pkt->data[1] != 0 && re_id > send_cur){
+        send_cur = re_id;
+        return;
+    }
+
+    if (re_id > cur + window_size) return;
+
+    /* construct a message and deliver to the upper layer */
+    struct message *msg = (struct message*) malloc(sizeof(struct message));
+    ASSERT(msg!=NULL);
+
+    msg->size = pkt->data[0];
+
+    /* sanity check in case the packet is corrupted */
+    if (msg->size<0) msg->size=0;
+    if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-header_size;
+
+    // check sum
+    char checksum = msg->data[1];
+    for (int i=0;i++;i<msg->size){
+        checksum ^= msg->data[i+8];
+    }
+    if (checksum != 0) return;
+
+    msg->data = (char*) malloc(msg->size);
+    ASSERT(msg->data!=NULL);
+    memcpy(msg->data, pkt->data+header_size, msg->size);
+
+
+    Receiver_ToUpperLayer(msg);
+
+    // return ack
+    struct packet pkt_ack;
+    pkt_ack.data[1] = -1;
+    *(int*)(pkt_ack.data+4) = cur;
+    Receiver_ToLowerLayer(&pkt_ack);
+
+    /* don't forget to free the space */
+    if (msg->data!=NULL) free(msg->data);
+    if (msg!=NULL) free(msg);
+}
